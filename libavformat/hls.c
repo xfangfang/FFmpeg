c2d62bff1c008c558db503c8f4765b0d2e97967a
xfangfang <2553041586@qq.com>
2022-01-30 01:22:52 +0800

    avformat/hls: Force mpegts demuxer

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -1960,7 +1960,6 @@ static int hls_read_header(AVFormatContext *s)
     /* Open the demuxer for each playlist */
     for (i = 0; i < c->n_playlists; i++) {
         struct playlist *pls = c->playlists[i];
-        char *url;
         ff_const59 AVInputFormat *in_fmt = NULL;
 
         if (!(pls->ctx = avformat_alloc_context())) {
@@ -1996,23 +1995,12 @@ static int hls_read_header(AVFormatContext *s)
         }
         ffio_init_context(&pls->pb, pls->read_buffer, INITIAL_BUFFER_SIZE, 0, pls,
                           read_data, NULL, NULL);
-        pls->ctx->probesize = s->probesize > 0 ? s->probesize : 1024 * 4;
-        pls->ctx->max_analyze_duration = s->max_analyze_duration > 0 ? s->max_analyze_duration : 4 * AV_TIME_BASE;
-        pls->ctx->interrupt_callback = s->interrupt_callback;
-        url = av_strdup(pls->segments[0]->url);
-        ret = av_probe_input_buffer(&pls->pb, &in_fmt, url, NULL, 0, 0);
-        if (ret < 0) {
-            /* Free the ctx - it isn't initialized properly at this point,
-             * so avformat_close_input shouldn't be called. If
-             * avformat_open_input fails below, it frees and zeros the
-             * context, so it doesn't need any special treatment like this. */
-            av_log(s, AV_LOG_ERROR, "Error when loading first segment '%s'\n", url);
-            avformat_free_context(pls->ctx);
-            pls->ctx = NULL;
-            av_free(url);
-            goto fail;
-        }
-        av_free(url);
+
+        void *iter = NULL;
+        while ((in_fmt = (ff_const59 AVInputFormat *)av_demuxer_iterate(&iter)))
+            if (strstr(in_fmt->name, "mpegts"))
+              break;
+
         pls->ctx->pb       = &pls->pb;
         pls->ctx->io_open  = nested_io_open;
         pls->ctx->flags   |= s->flags & ~AVFMT_FLAG_CUSTOM_IO;
